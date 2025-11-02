ada542edd65c4ee095e0b353d6fc9df925c5e0ce
DaoChaShao <27882272@qq.com>
2025-11-02 18:25:48 +0800

    fix: refactor data preprocessing and dataset preparation in main.py; add class verification function

--- main.py
+++ main.py
@@ -6,49 +6,98 @@
 # @File     :   main.py
 # @Desc     :   
 
+from numpy import array, sum as np_sum
 from pathlib import Path
 from PIL import Image
+from pandas import DataFrame, read_csv
 from random import randint
 from torch import optim, nn
 from torchvision import transforms
 
-from dataseter.unet import UNetDataset
-from models.segment import UNetDoubleConvModel
-from trainers.unet_seg import UNetSegmentationTrainer
+from dataseter.unet_seg_setter import MaskToClassID, UNetDataset
+from models.unet_seg_model import UNetDoubleConvModel
+from trainers.train_unet_seg import UNetSegmentationTrainer
 from utils.config import CONFIG
-from utils.helper import load_data_paths
+from utils.helper import load_data_paths, Timer
+from utils.highlighter import red, green
 from utils.PT import TorchDataLoader
+
+
+def verify_classes_convention(class2id: dict, mask: Image.Image) -> None:
+    """ Check the classes in the mask """
+    mask = array(mask)
+
+    # Calculate unique classes in the mask
+    mask_classes = set(mask.flatten().tolist())
+    unique_classes = set(class2id.values())
+    mapped_classes = mask_classes.intersection(unique_classes)
+    print(f"There are {len(unique_classes)} classes.")
+    print(f"There are {green(len(mapped_classes))} classes in the mask found in the class2id mapping.")
+    print(f"There are {red(len(unique_classes - mapped_classes))} classes in the mask not mapped in class2id.")
 
 
 def preprocess_data():
     """ Preprocess the data """
-    path_train = Path(CONFIG.FILEPATHS.DATASET_TRAIN)
-    path_valid = Path(CONFIG.FILEPATHS.DATASET_TEST)
-    # print(path_train)
-    # print(path_valid)
+    base_train = Path(CONFIG.FILEPATHS.DATASET_TRAIN)
+    base_valid = Path(CONFIG.FILEPATHS.DATASET_VALID)
+    # print(base_train)
+    # print(base_valid)
 
-    train: tuple[list[str], list[str]] = load_data_paths(path_train)
-    valid: tuple[list[str], list[str]] = load_data_paths(path_valid)
-    # print(raw_train[0][:3])
-    # print(raw_train[1][:3])
+    paths_train: tuple[list[str], list[str]] = load_data_paths(base_train)
+    paths_valid: tuple[list[str], list[str]] = load_data_paths(base_valid)
+    # print(len(paths_train[0]), len(paths_valid[0]))
+    # print(paths_train[0][:3])
+    # print(paths_train[1][:3])
+    # print(paths_valid[0][:3])
+    # print(paths_valid[1][:3])
 
-    index = randint(0, len(train) - 1)
-    img_train = Image.open(train[0][index])
-    mask_train = Image.open(train[1][index])
-    print(f"The image info in train dataset is {img_train.size} {img_train.mode}")
-    print(f"The mask info in train dataset is {mask_train.size} {mask_train.mode}")
+    # index_train = randint(0, len(paths_train[0]) - 1)
+    # img_train = Image.open(paths_train[0][index_train])
+    # mask_train = Image.open(paths_train[1][index_train])
+    # print(f"Random train index selected: {index_train}")
+    # print(f"The image info in train dataset is {img_train.size} {img_train.mode}")
+    # print(f"The mask info in train dataset is {mask_train.size} {mask_train.mode}")
 
-    img_valid = Image.open(valid[0][index])
-    mask_valid = Image.open(valid[1][index])
-    print(f"The image info in valid dataset is {img_valid.size} {img_valid.mode}")
-    print(f"The mask info in valid dataset is {mask_valid.size} {img_valid.mode}")
+    # index_valid = randint(0, len(paths_valid[0]) - 1)
+    # img_valid = Image.open(paths_valid[0][index_valid])
+    # mask_valid = Image.open(paths_valid[1][index_valid])
+    # print(f"Random valid index selected: {index_valid}")
+    # print(f"The image info in test dataset is {img_valid.size} {img_valid.mode}")
+    # print(f"The mask info in test dataset is {mask_valid.size} {img_valid.mode}")
 
-    return train, valid
+    classes: DataFrame = read_csv(CONFIG.FILEPATHS.CLASSES_DICTIONARY)
+    # print(f"The number of segmentation classes: {len(classes)}")
+    # print(f"Segmentation classes dictionary:\n{classes}")
+    class2id: dict[tuple[int, int, int], int] = {}
+    for i in range(len(classes)):
+        row = classes.iloc[i]
+        class2id[(
+            int(row["r"]),
+            int(row["g"]),
+            int(row["b"])
+        )] = i
+    # print(f"Total number of classes: {len(class2id)}")
+    # print(f"Class to ID mapping:\n{class2id}")
+
+    return paths_train, paths_valid, class2id
 
 
 def prepare_dataset():
-    train, valid = preprocess_data()
-    print(len(train[0]), len(valid[0]))
+    # preprocess_data()
+    paths_train, paths_valid, class2id = preprocess_data()
+
+    # Check the mask metric
+    # index_check: int = randint(0, len(paths_train[1]) - 1)
+    # print(paths_train[1][index_check])
+    # mask_check: Image.Image = Image.open(paths_train[1][index_check]).convert("RGB")
+    # converter = MaskToClassID(class2id)
+    # mask_converted: Image.Image = converter(mask_check)
+    # print(f"Random index selected for mask check: {index_check}")
+    # print(f"Original mask size: {mask_check.size}, mode: {mask_check.mode}")
+    # print(f"Converted mask size: {mask_converted.size}, mode: {mask_converted.mode}")
+    # print(array(mask_check))
+    # print(array(mask_converted))
+    # verify_classes_convention(class2id, mask_converted)
 
     # Setup image enhancements
     img_transformer = transforms.Compose([
@@ -63,31 +112,32 @@ def prepare_dataset():
     ])
     mask_transformer = transforms.Compose([
         transforms.Resize((512, 512)),
-        transforms.ToTensor(),
     ])
 
     # Setup datasets
     dataset_train = UNetDataset(
-        image_paths=train[0],
-        mask_paths=train[1],
+        image_paths=paths_train[0],
+        mask_paths=paths_train[1],
+        class2id=class2id,
         img_transformer=img_transformer,
         mask_transformer=mask_transformer
     )
     dataset_valid = UNetDataset(
-        image_paths=valid[0],
-        mask_paths=valid[1],
+        image_paths=paths_valid[0],
+        mask_paths=paths_valid[1],
+        class2id=class2id,
         img_transformer=img_transformer,
         mask_transformer=mask_transformer
     )
-    index_train: int = randint(0, len(dataset_train) - 1)
-    index_valid: int = randint(0, len(dataset_valid) - 1)
-    img_train, mask_train = dataset_train[index_train]
-    img_valid, mask_valid = dataset_valid[index_valid]
-    # print(index_train, index_valid)
-    # print(img_train, img_valid)
-    print(f"Transformed train image shape: {img_train.shape}, mask shape: {mask_train.shape}")
-    print(f"Transformed valid image shape: {img_valid.shape}, mask shape: {mask_valid.shape}")
+    # index_train: int = randint(0, len(dataset_train) - 1)
+    # index_valid: int = randint(0, len(dataset_valid) - 1)
+    # img_train, mask_train = dataset_train[index_train]
+    # img_valid, mask_valid = dataset_valid[index_valid]
+    # print(f"Transformed train image shape: {img_train.shape}, mask shape: {mask_train.shape}")
+    # print(f"Transformed test image shape: {img_valid.shape}, mask shape: {mask_valid.shape}")
+    # print(mask_train.numpy(), mask_valid.numpy(), sep="\n")
 
+    # Set up dataloader
     dataloader_train = TorchDataLoader(
         dataset=dataset_train,
         batch_size=CONFIG.PREPROCESSOR.BATCHES,
@@ -98,28 +148,35 @@ def prepare_dataset():
         batch_size=CONFIG.PREPROCESSOR.BATCHES,
         is_shuffle=CONFIG.PREPROCESSOR.IS_SHUFFLE,
     )
+
     # print(f"Number of training batches: {len(dataloader_train)}")
     # print(f"Number of validation batches: {len(dataloader_valid)}")
 
-    return dataloader_train, dataloader_valid
+    return dataloader_train, dataloader_valid, class2id
 
 
 def main() -> None:
     """ Main Function """
-    train, valid = prepare_dataset()
+    with Timer("Training UNet Segmentation Model"):
+        # prepare_dataset()
+        train, valid, class2id = prepare_dataset()
 
-    # Set up a model
-    channels, height, width = train[0][0].shape
-    print(channels, height, width)
-    model = UNetDoubleConvModel(channels, CONFIG.UNET_PARAMS.SEG_CLASSES, height, width, )
+        # Set up a model
+        channels, height, width = train[0][0].shape
+        # print(channels, height, width)
+        model = UNetDoubleConvModel(channels, CONFIG.UNET_PARAMS.SEG_CLASSES, height, width)
 
-    # Set up an optimiser and loss function
-    optimiser = optim.AdamW(model.parameters(), lr=CONFIG.HYPERPARAMETERS.ALPHA)
-    criterion = nn.CrossEntropyLoss()
+        # Set up an optimiser and loss function
+        optimiser = optim.AdamW(model.parameters(), lr=CONFIG.HYPERPARAMETERS.ALPHA)
+        criterion = nn.CrossEntropyLoss()
 
-    # Initialise a trainer
-    trainer = UNetSegmentationTrainer(model, optimiser, criterion, CONFIG.HYPERPARAMETERS.ACCELERATOR)
-    trainer.fit(train, valid, CONFIG.HYPERPARAMETERS.EPOCHS, CONFIG.FILEPATHS.MODEL)
+        # Initialise a trainer
+        trainer = UNetSegmentationTrainer(
+            model, optimiser, criterion,
+            len(class2id),
+            CONFIG.HYPERPARAMETERS.ACCELERATOR
+        )
+        trainer.fit(train, valid, CONFIG.HYPERPARAMETERS.EPOCHS, CONFIG.FILEPATHS.MODEL)
 
 
 if __name__ == "__main__":
